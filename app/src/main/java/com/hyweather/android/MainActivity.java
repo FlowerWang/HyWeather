1b1b01cfcaea30fe99163924eb4a1ed7bf564eca
wangshuai <799088606@qq.com>
2019-06-12 00:35:02 +0800

    配置数据库和实体类

--- app/src/main/java/com/hyweather/android/MainActivity.java
+++ app/src/main/java/com/hyweather/android/MainActivity.java
@@ -3,11 +3,15 @@ package com.hyweather.android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.litepal.LitePal;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //初始化LitePal
+        LitePal.initialize(this);
         setContentView(R.layout.activity_main);
     }
 }
